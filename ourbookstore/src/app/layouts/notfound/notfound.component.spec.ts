0bf882aa9ba8dcc491e128889303eb51f18bbbb7
Likeur <jacksimba28@gmail.com>
2024-10-22 14:00:26 +0200

    fixe logIn and signIn and adding a notfound component

---
+++ ourbookstore/src/app/layouts/notfound/notfound.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotfoundComponent } from './notfound.component';
+
+describe('NotfoundComponent', () => {
+  let component: NotfoundComponent;
+  let fixture: ComponentFixture<NotfoundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NotfoundComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NotfoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
